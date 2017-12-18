70aba7e9685ec9bb57bcdc58bce712607cbaebef
Glitch (quaint-bun) <none>
2017-12-18 06:06:01 +0000

    🚆🏜 Checkpoint program.js:566533

--- program.js
+++ program.js
@@ -1,12 +1,7 @@
 var express = require('express')
-var fs = require('fs')
 var app = express()
-app.get('/books', function(req, res){
-    fs.readFile(process.argv[3], (error, data) => {
-        if (error) {
-            console.log(error)
-        }
-        res.send(JSON.stringify(JSON.parse(data)));
-    })
+app.get('/', function(req, res){
+    console.log('Working');
 });
-app.listen(process.argv[2])
+app.listen(3000)
+//app.listen(process.argv[2])
